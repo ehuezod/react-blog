9bb1bfd965b9464603dd263a43c71ed0314adc45
ehuezod <ehuezod@ucenfotec.ac.cr>
2022-12-14 16:45:25 -0600

    adds react router and routes for createpost/register/viewposts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Posts from "./components/Posts";
+import CreateUser from "./components/CreateUser";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/newuser" element={<CreateUser />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Posts.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Posts() {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <p>mypostst!</p>
+    </div>
+  );
+}
